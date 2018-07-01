da17d8d10de1e29e690979d58a102994db58a925
InnaFr <39226073+InnaFr@users.noreply.github.com>
2018-07-01 14:44:04 +0800

    Еще раз бд

--- SocialMediaInformationAggregator/SocialMediaInformationAggregator/DatabaseInteraction/PeopleFromDb.cs
+++ SocialMediaInformationAggregator/SocialMediaInformationAggregator/DatabaseInteraction/PeopleFromDb.cs
@@ -19,14 +19,12 @@ namespace SocialMediaInformationAggregator.DatabaseInteraction
             connectionString = "Data Source=LAPTOP-8FE5V0OM\\SQLEXPRESS;Initial Catalog=SMIA;Integrated Security=True";
         }
 
-
-        public static List<FindPeople.PersonInformation> GetFoundPeople(string login)
+        private static List<FindPeople.PersonInformation> GetFoindPeopleFromNetworks(string login, string TableName)
         {
-            /// возвращает данные о найденных людях для определенного пользователя
             ConnectToDatabase();
             SqlConnection conn = new SqlConnection(connectionString);
             conn.Open();
-            string query = String.Format("SELECT * FROM FoundPersons WHERE UserLogin=@a");
+            string query = String.Format("SELECT * FROM " + TableName+ " WHERE UserLogin=@a");
             List<FindPeople.PersonInformation> list = new List<FindPeople.PersonInformation>();
             using (SqlCommand comm = new SqlCommand(query, conn))
             {
@@ -40,12 +38,14 @@ namespace SocialMediaInformationAggregator.DatabaseInteraction
                         hum.Name = reader.GetValue(1).ToString();
                         hum.LastName = reader.GetValue(2).ToString();
                         hum.YearOfBirth = (int)reader.GetValue(3);
+
                         List<string> lst = new List<string>();
                         lst.Add(reader.GetValue(4).ToString());
                         hum.Cities = SeparateElements(lst);
                         lst.Clear();
                         lst.Add(reader.GetValue(5).ToString());
                         hum.Education = SeparateElements(lst);
+
                         list.Add(hum);
                     }
                 }
@@ -53,13 +53,29 @@ namespace SocialMediaInformationAggregator.DatabaseInteraction
             return list;
         }
 
-        public static List<string> GetRowInformFromDb(string login, int k)
+        public static List<FindPeople.PersonInformation> GetFoundPeople(string login)
+        {
+            List<FindPeople.PersonInformation> PersonsFromVK = GetFoindPeopleFromNetworks(login, "FoundPersonsVK");
+            List<FindPeople.PersonInformation> PersonsFromOK = GetFoindPeopleFromNetworks(login, "FoundPersonOK");
+            List<FindPeople.PersonInformation> CommonPersons = PersonsFromVK.Concat(PersonsFromOK).ToList();
+            return CommonPersons;
+        }
+
+        private static List<string> GetRowInformFromDb(string login, int k)
+        {
+            List<string> informFromVK = GetRowInformFromDbNetwork(login, "FoundPersonsVK", k);
+            List<string> informFromOK = GetRowInformFromDbNetwork(login, "FoundPersonsOK", k);
+            List<string> CommonInform = informFromVK.Concat(informFromOK).Distinct().ToList();
+            return CommonInform;
+        }
+
+        private static List<string> GetRowInformFromDbNetwork(string login, string TableName, int k)
         {
             /// возвращает список значений определенного столбца таблицы для заданного пользователя
             ConnectToDatabase();
             SqlConnection conn = new SqlConnection(connectionString);
             conn.Open();
-            string query = String.Format("SELECT * FROM FoundPersons WHERE UserLogin=@a");
+            string query = String.Format("SELECT * FROM "+TableName+" WHERE UserLogin=@a");
             List<string> list = new List<string>();
             using (SqlCommand comm = new SqlCommand(query, conn))
             {
@@ -77,8 +93,6 @@ namespace SocialMediaInformationAggregator.DatabaseInteraction
         }
 
 
-
-
         public static List<string> LastNameFoundPeople(string login, int count = -1, string startWith = "")
         {
             List<string> lastNamesList = new List<string>();
@@ -89,7 +103,7 @@ namespace SocialMediaInformationAggregator.DatabaseInteraction
                     IEnumerable<string> buf = GetRowInformFromDb(login, 2).Where(a => a.StartsWith(startWith)).OrderBy(a => a).Take(count);
                     lastNamesList = buf.ToList();
                 }
-                else if (count !=-1 && startWith=="")
+                else if (count != -1 && startWith == "")
                 {
                     IEnumerable<string> buf = GetRowInformFromDb(login, 2).OrderBy(a => a).Take(count);
                     lastNamesList = buf.ToList();
@@ -132,7 +146,7 @@ namespace SocialMediaInformationAggregator.DatabaseInteraction
                     IEnumerable<string> buf = GetRowInformFromDb(login, 1).OrderBy(a => a);
                     firstNamesList = buf.ToList();
                 }
-                else if (count!=-1 && startWith =="")
+                else if (count != -1 && startWith == "")
                 {
                     IEnumerable<string> buf = GetRowInformFromDb(login, 1).OrderBy(a => a).Take(count);
                     firstNamesList = buf.ToList();
@@ -143,7 +157,7 @@ namespace SocialMediaInformationAggregator.DatabaseInteraction
             {
                 throw new Exception("Введено некорректное значение!");
             }
-            
+
         }
 
         public static List<string> SeparateElements(List<string> mas)
@@ -151,9 +165,11 @@ namespace SocialMediaInformationAggregator.DatabaseInteraction
             List<string> elements = new List<string>();
             foreach (string a in mas)
             {
-                List<string> buf = a.Split(',').ToList();
-                foreach (string p in buf)
-                    elements.Add(p);
+                string[] trySort = a.Split(',');
+                foreach (string p in trySort)
+                {
+                    elements.Add(p.TrimStart(' '));
+                }
             }
             elements = elements.Distinct().ToList();
             return elements;
@@ -161,7 +177,7 @@ namespace SocialMediaInformationAggregator.DatabaseInteraction
         public static List<string> CityFoundPeople(string login, int count = -1, string startWith = "")
         {
             List<string> cities = new List<string>();
-            if (!String.IsNullOrWhiteSpace(login) && (count > 0 || count==-1))
+            if (!String.IsNullOrWhiteSpace(login) && (count > 0 || count == -1))
             {
                 if (count > 0 && startWith != "")
                 {
@@ -171,14 +187,14 @@ namespace SocialMediaInformationAggregator.DatabaseInteraction
                 else if (count == -1 && startWith != "")
                 {
                     List<string> AllElements = GetRowInformFromDb(login, 4);
-                    cities = SeparateElements(AllElements).OrderBy(a => a).ToList();
+                    cities = SeparateElements(AllElements).ToList().Where(a=>a.StartsWith(startWith)).OrderBy(a => a).ToList();
                 }
                 else if (count == -1 && startWith == "")
                 {
                     List<string> AllElements = GetRowInformFromDb(login, 4);
-                    cities = SeparateElements(AllElements).OrderBy(a=>a).ToList();
+                    cities = SeparateElements(AllElements).OrderBy(a => a).ToList();
                 }
-                else if (count!=-1 && startWith == "")
+                else if (count > -1 && startWith == "")
                 {
                     List<string> AllElements = GetRowInformFromDb(login, 4);
                     cities = SeparateElements(AllElements).OrderBy(a => a).Take(count).ToList();
@@ -200,22 +216,21 @@ namespace SocialMediaInformationAggregator.DatabaseInteraction
                 if (count > 0 && startWith != "")
                 {
                     List<string> AllElements = GetRowInformFromDb(login, 5);
-                    education = SeparateElements(AllElements).Where(a => a.StartsWith(startWith)).OrderBy(a => a).Take(count).ToList();
+                    education = SeparateElements(AllElements).Select(a=>a).Where(a => a.StartsWith(startWith)).OrderBy(a => a).Take(count).ToList();
                 }
                 else if (count == -1 && startWith != "")
                 {
                     List<string> AllElements = GetRowInformFromDb(login, 5);
-                    IEnumerable<string> res = SeparateElements(AllElements).Where(a => a.StartsWith(startWith)).OrderBy(a => a);
+                    IEnumerable<string> res = SeparateElements(AllElements).Select(a => a).Where(a => a.StartsWith(startWith)).OrderBy(a => a);
                     education = res.ToList();
                 }
                 else if (count == -1 && startWith == "")
                 {
                     List<string> AllElements = GetRowInformFromDb(login, 5);
-                    List<string> smth = SeparateElements(AllElements);
-                    IEnumerable<string> res = smth.OrderBy(a => a);
-                     education = res.ToList();
+                    IEnumerable<string> res = SeparateElements(AllElements).OrderBy(a => a);
+                    education = res.ToList();
                 }
-                else if (count> 1 && startWith == "")
+                else if (count > 1 && startWith == "")
                 {
                     List<string> AllElements = GetRowInformFromDb(login, 5);
                     education = SeparateElements(AllElements).OrderBy(a => a).Take(count).ToList();
@@ -228,7 +243,23 @@ namespace SocialMediaInformationAggregator.DatabaseInteraction
             }
         }
 
-        public static void AddFoundPerson(FindPeople.PersonInformation pers)
+        public static void AddFoundPerson(FindPeople.PersonInformation persVK = null, FindPeople.PersonInformation persOK = null)
+        ///Vk = true, значит, ссылка из ВК, в противном случае - из ОК
+        {
+            if (persVK != null || persOK != null)
+            {
+                if (persVK != null)
+                {
+                    InsertIntoNetwork(persVK, "FoundPersonsVK");
+                }
+                if (persOK != null)
+                {
+                    InsertIntoNetwork(persOK, "FoundPersonsOK");
+                }
+            }
+        }
+
+        private static void InsertIntoNetwork(FindPeople.PersonInformation pers, string tableName)
         {
             if (!String.IsNullOrEmpty(pers.Name)
                 && !String.IsNullOrEmpty(pers.LastName)
@@ -248,8 +279,8 @@ namespace SocialMediaInformationAggregator.DatabaseInteraction
                     ConnectToDatabase();
                     SqlConnection conn = new SqlConnection(connectionString);
                     conn.Open();
-                    string query = String.Format("INSERT INTO FoundPersons " +
-                        "(UserLogin, FirstName, LastName, BirthData, Cities, " +
+                    string query = String.Format("INSERT INTO " + tableName +
+                        " (UserLogin, FirstName, LastName, BirthData, Cities, " +
                         "Education, LinkVK) " +
                         "VALUES (@login, @name, @lastName, @birthData, @cities," +
                         "@education, @linkVK)");
@@ -279,8 +310,7 @@ namespace SocialMediaInformationAggregator.DatabaseInteraction
                 throw new Exception("Обязательные поля не заполнены!");
             }
         }
-
-        public static string MakeEducationRow(FindPeople.PersonInformation pers)
+        private static string MakeEducationRow(FindPeople.PersonInformation pers)
         {
             string education = "";
             foreach (string a in pers.Education)
@@ -294,7 +324,7 @@ namespace SocialMediaInformationAggregator.DatabaseInteraction
             return education;
         }
 
-        public static string MakeCityRow(FindPeople.PersonInformation pers)
+        private static string MakeCityRow(FindPeople.PersonInformation pers)
         {
             string cities = "";
             foreach (string a in pers.Education)
