b1084b10f8ef98c0178dd94b5ca36e5713a99fef
InnaFr <39226073+InnaFr@users.noreply.github.com>
2018-07-05 17:36:31 +0800

    страничка с вопросами

--- SocialMediaInformationAggregator/SocialMediaInformationAggregator/QuestionPage.xaml.cs
+++ SocialMediaInformationAggregator/SocialMediaInformationAggregator/QuestionPage.xaml.cs
@@ -30,25 +30,34 @@ namespace SocialMediaInformationAggregator
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            SqlConnection conn = new SqlConnection(App.ConnectionString);
-            try
+            if (!String.IsNullOrWhiteSpace(textBoxHobby.Text.Trim(' '))
+                && !String.IsNullOrWhiteSpace(textBoxPet.Text.Trim(' '))
+                && !String.IsNullOrWhiteSpace(textBoxSer.Text.Trim(' ')))
             {
-                conn.Open();
+                SqlConnection conn = new SqlConnection(App.ConnectionString);
+                try
+                {
+                    conn.Open();
 
-                string query = string.Format("UPDATE Users SET [FirstQuestion] = '{0}', [SecondQuestion]='{1}', [ThirdQuestion]='{2}' " +
-                                             "WHERE (Login='{3}')",
-                                             textBoxSer.Text.ToLower().Trim(' '), 
-                                             textBoxPet.Text.ToLower().Trim(' '), 
-                                             textBoxHobby.Text.ToLower().Trim(' '), 
-                                             App.LoginGlobalVeryForMethod.ToLower().Trim(' '));
-                SqlCommand comm = new SqlCommand(query, conn);
-                comm.ExecuteNonQuery();
-                conn.Close();
-                this.NavigationService.Navigate(new Uri("InputPage.xaml", UriKind.Relative));
+                    string query = string.Format("UPDATE Users SET [FirstQuestion] = '{0}', [SecondQuestion]='{1}', [ThirdQuestion]='{2}' " +
+                                                 "WHERE (Login='{3}')",
+                                                 textBoxSer.Text.ToLower().Trim(' '),
+                                                 textBoxPet.Text.ToLower().Trim(' '),
+                                                 textBoxHobby.Text.ToLower().Trim(' '),
+                                                 App.LoginGlobalVeryForMethod.ToLower().Trim(' '));
+                    SqlCommand comm = new SqlCommand(query, conn);
+                    comm.ExecuteNonQuery();
+                    conn.Close();
+                    this.NavigationService.Navigate(new Uri("InputPage.xaml", UriKind.Relative));
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
             }
-            catch (Exception ex)
+            else
             {
-                MessageBox.Show(ex.Message);
+                MessageBox.Show("Не все поля заполнены!");
             }
         }
     }
